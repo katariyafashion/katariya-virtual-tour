64005be0f81f0dbf377a27574182e4dcf48c886c
Katariya Fashion <katariyafashion123@gmail.com>
2025-07-09 16:23:51 +0530

    new

--- js/pano.js
+++ js/pano.js
@@ -1,18 +1,11 @@
-// Katariya Fashion Virtual Tour - pano.js
-// Requires marzipano.js in the HTML
-
 var viewer = new Marzipano.Viewer(document.getElementById('pano'));
 
 var scenes = {};
 
 function createScene(id, title) {
-  var source = Marzipano.ImageUrlSource.fromString(
-    "scenes/" + id + ".jpg"
-  );
-  var geometry = new Marzipano.EquirectGeometry([{
-    width: 4000
-  }]);
-  var limiter = Marzipano.RectilinearView.limit.traditional(1024, 100*Math.PI/180);
+  var source = Marzipano.ImageUrlSource.fromString("scenes/" + id + ".jpg");
+  var geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
+  var limiter = Marzipano.RectilinearView.limit.traditional(1024, 100 * Math.PI / 180);
   var view = new Marzipano.RectilinearView(null, limiter);
   var scene = viewer.createScene({
     source: source,
@@ -28,22 +21,28 @@ function createScene(id, title) {
   };
 }
 
-createScene("outside", "Outside View");
-createScene("mainhall", "Main Hall");
-createScene("dress_material", "Dress Material Section");
-createScene("pant_set_reception", "Pant Set Reception");
-createScene("pant_set_department", "Pant Set Department");
-createScene("kurtis_upstairs", "Short Kurtis (Upstairs)");
-createScene("kurti_downstairs", "Long Kurtis (Downstairs)");
+var sceneList = [
+  "outside",
+  "mainhall",
+  "dress_material",
+  "pant_set_reception",
+  "pant_set_department",
+  "kurtis_upstairs",
+  "kurti_downstairs"
+];
 
-// Start with 'outside'
+sceneList.forEach(function(id) {
+  createScene(id, id);
+});
+
+// Load first scene
 scenes["outside"].scene.switchTo();
 
-// ✅ Global scene switch function for navigation buttons
+// Make navigation function global
 window.gotoScene = function(id) {
   if (scenes[id]) {
     scenes[id].scene.switchTo();
   } else {
-    console.warn("Scene not found:", id);
+    alert("Scene not found: " + id);
   }
 };
